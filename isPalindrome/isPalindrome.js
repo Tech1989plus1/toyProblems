6e53c040cb15d35d1ccdb27f1eebb2151e7090c6
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-01-18 12:22:40 -0800

    Palindrome

---
+++ isPalindrome/isPalindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+const isPalindrome = function(x) {
+  if (x === 0) {
+    return true;
+  }
+  
+  if (typeof x === 'number' && Math.sign(x) !== -1) {
+    x = x + "";
+    const reverse = Number(x.split("").reverse().join(''));
+
+    if ( (x / reverse) === 1) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//module.exports = isPalindrome;
+console.log(isPalindrome(121))
