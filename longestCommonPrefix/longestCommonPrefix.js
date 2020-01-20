7d1a18b7754ce1089172d574dbb02a9f63c61e18
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-01-19 17:10:36 -0800

    toyPro

--- longestCommonPrefix/longestCommonPrefix.js
+++ longestCommonPrefix/longestCommonPrefix.js
@@ -6,7 +6,7 @@
  const longestCommonPrefix = (strs) => {
     var result = "";
     
-    if (Array.isArray(strs)) {
+    if (Array.isArray(strs) && strs.length !== 0) {
       const word = strs[0];
 
       word.split("").map((char) => {
@@ -14,6 +14,15 @@
         for (let i = 1; i < strs.length; i++) {
           if (!strs[i].includes(char)){
             bool = false;
+          } else {
+            const indexOf = strs[i].indexOf(char);
+            var str = strs[i].split('');
+            var temp = str[str.length - 1];
+            str[str.length - 1] = str[indexOf];
+            str[indexOf] = temp;
+            str.pop();
+            
+            
           }
         }
 
@@ -27,4 +36,6 @@
     return result;
 };
 
-module.exports = longestCommonPrefix;
+//module.exports = longestCommonPrefix;
+
+console.log(longestCommonPrefix(["aa","ab"]))
--- longestCommonPrefix/test.js
+++ longestCommonPrefix/test.js
@@ -1,1 +1,17 @@
-const expect 
+const expect = require('chai').expect;
+
+describe('Longest Common Prefix', () => {
+  const longestCommonPrefix = require('./longestCommonPrefix.js');
+
+  it(`If there is no common prefix, return an empty string "".`, () => {
+    expect(longestCommonPrefix(["dog","racecar","car"])).to.equal("");
+  });
+
+  it(`Common prefix should return "fl"`, () => {
+    expect(longestCommonPrefix(["flower","flow","flight"])).to.equal("fl");
+  });
+
+  it(`Common prefix should return "rick"`, () => {
+    expect(longestCommonPrefix(["derick","rick","ricky", "kicr"])).to.equal("rick");
+  });
+});
