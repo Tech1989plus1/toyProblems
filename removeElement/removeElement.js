cfdd603450845639a7db18ec63d8abeb3ee23205
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-01-22 09:36:53 -0800

    if (Array.isArray(nums)) {

--- removeElement/removeElement.js
+++ removeElement/removeElement.js
@@ -3,6 +3,24 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-    
+var removeElement = (nums, val) => {
+  if (Array.isArray(nums)) {
+    let i = 0;
+
+    while (i < nums.length) {
+      const num = nums[i];
+      if (num === val) {
+        for (let j = i; j < nums.length - 1; j++) {
+          nums[j] = nums[j+1];
+        }
+        nums.pop()
+      } else {
+        i++;
+      }
+    }
+
+  }
+  return nums.length;
 };
+
+console.log(removeElement([0,1,2,2,3,0,4,2], 2))
