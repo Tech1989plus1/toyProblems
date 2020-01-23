2889cda24f0b0c6dcb3ec385210df56477936d89
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-01-23 09:55:40 -0800

    tp

--- strStr/strStr.js
+++ strStr/strStr.js
@@ -8,16 +8,25 @@
   if (typeof haystack === 'string' && typeof needle === 'string') {
     haystack = haystack.split('');
     needle = needle.split('');
+
+    if ((haystack.length === 0 && needle.length === 0) || needle.length === 0) return 0;
     
-    haystack.map((char, key) => {
+    for (let index = 0; index < haystack.length; +index++) {
+      const char = haystack[index];
+      
       if (char === needle[0]) {
-        let j = 1;
-        let i = key + 1;
-        
+        var bool = true;
+        var keyIndex = index + 1;
+        for (let i = 1; i < needle.length; i++) {
+          if (needle[i] !== haystack[keyIndex]) bool = false;
+          keyIndex++;
+        }
+  
+        if (bool) return index;
       }
-    });
+    }
   }
   return -1;
 };
 
-console.log(strStr("hello","ll"));
+console.log(strStr("aaa","aaaa"));
