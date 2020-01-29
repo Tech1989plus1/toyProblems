7c213813c5ede80abe954dc645302501987a0006
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-01-29 11:06:19 -0800

    tp

--- plusOne/plusOne.js
+++ plusOne/plusOne.js
@@ -4,5 +4,17 @@
  */
 
 var plusOne = (digits) => {
-    
+  var numS = "";
+
+  digits.map((num) => numS += num.toString());
+
+  numS = (Number(numS) + 1).toString().split("");
+
+  numS.map((num, key) => numS[key] = Number(num));
+  
+  return numS;
 };
+
+var nums = plusOne([9,9]);
+
+console.log(nums);
