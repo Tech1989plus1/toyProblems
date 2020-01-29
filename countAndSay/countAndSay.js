3dd67b90f08f904175902ab1c6c77cb176dd0326
Derrick Marshall <marshallderrickdnm@gmail.com>
2020-01-29 10:34:50 -0800

    plusOne

---
+++ countAndSay/countAndSay.js
@@ -0,0 +1,51 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+
+var countAndSay = (n) => {
+  var result = "";
+
+  if (n >= 1 && n <= 30) {
+    if (n === 1) return n.toString();
+
+  }
+};
+
+console.log(countAndSay(31));
+
+phone screen questions:
+  Wall of catch phases you can just pick from.
+  Achror as a software engireer with a back ground in ""
+  this when I got the engireer bug
+  Why tech?
+    Problem sovling process
+    Front End, CS fundamental, Working with others wanted to be in a room in front of engieers
+
+  Can you tell me about HR?
+    Tech + Social
+    Learning how to Learn
+    Loving the projects
+    
+  What are you thinking for what are you looking for?
+    The culurt
+  
+  When you say you want room to grow?
+    What is the team currently working on?
+    
+  What will you teammates say about you?
+    Outgoing
+    Comment
+    Support
+
+    I am sure we are a goof fit, and we 
+
+
+
+nick: 
+  
+
+elaine:
+  Talking about here personal experience
+  Team is going, but wanted to work with more SF
+
